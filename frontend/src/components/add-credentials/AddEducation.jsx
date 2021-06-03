ad1afd698de1cabc76d5df4b79f486a671a39605
Faiq <faiq.019@gmail.com>
2021-06-03 16:03:29 +0500

    Add experience functionality

--- frontend/src/components/add-credentials/AddEducation.jsx
+++ frontend/src/components/add-credentials/AddEducation.jsx
@@ -54,96 +54,96 @@ const AddEducation = (props) => {
   }
 
   return (
-      <div className="add-education">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <Link to="/dashboard" className="btn btn-light">
-                Go Back
-              </Link>
-              <h1 className="display-4 text-center">Add Education</h1>
-              <p className="lead text-center">
-                Add any school, bootcamp, etc that you have attended
-              </p>
-              <small className="d-block pb-3">* = required fields</small>
-              <form onSubmit={onSubmit}>
-                <TextFieldGroup
-                  placeholder="* School"
-                  name="school"
-                  value={state.school}
-                  onChange={onChange}
-                  error={errors.school}
-                />
-                <TextFieldGroup
-                  placeholder="* Degree or Certification"
-                  name="degree"
-                  value={state.degree}
-                  onChange={onChange}
-                  error={errors.degree}
-                />
-                <TextFieldGroup
-                  placeholder="* Field of Study"
-                  name="fieldofstudy"
-                  value={state.fieldofstudy}
-                  onChange={onChange}
-                  error={errors.fieldofstudy}
-                />
-                <h6>From Date</h6>
-                <TextFieldGroup
-                  name="from"
-                  type="date"
-                  value={state.from}
-                  onChange={onChange}
-                  error={errors.from}
-                />
-                <h6>To Date</h6>
-                <TextFieldGroup
-                  name="to"
-                  type="date"
-                  value={state.to}
-                  onChange={onChange}
-                  error={errors.to}
-                  disabled={state.disabled ? 'disabled' : ''}
-                />
-                <div className="form-check mb-4">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    name="current"
-                    value={state.current}
-                    checked={state.current}
-                    onChange={onCheck}
-                    id="current"
-                  />
-                  <label htmlFor="current" className="form-check-label">
-                    Current Job
-                  </label>
-                </div>
-                <TextAreaFieldGroup
-                  placeholder="Program Description"
-                  name="description"
-                  value={state.description}
-                  onChange={onChange}
-                  error={errors.description}
-                  info="Tell us about the program that you were in"
-                />
+    <div className="add-education">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <Link to="/dashboard" className="btn btn-light">
+              Go Back
+            </Link>
+            <h1 className="display-4 text-center">Add Education</h1>
+            <p className="lead text-center">
+              Add any school, bootcamp, etc that you have attended
+            </p>
+            <small className="d-block pb-3">* = required fields</small>
+            <form onSubmit={onSubmit}>
+              <TextFieldGroup
+                placeholder="* School"
+                name="school"
+                value={state.school}
+                onChange={onChange}
+                error={errors.school}
+              />
+              <TextFieldGroup
+                placeholder="* Degree or Certification"
+                name="degree"
+                value={state.degree}
+                onChange={onChange}
+                error={errors.degree}
+              />
+              <TextFieldGroup
+                placeholder="* Field of Study"
+                name="fieldofstudy"
+                value={state.fieldofstudy}
+                onChange={onChange}
+                error={errors.fieldofstudy}
+              />
+              <h6>From Date</h6>
+              <TextFieldGroup
+                name="from"
+                type="date"
+                value={state.from}
+                onChange={onChange}
+                error={errors.from}
+              />
+              <h6>To Date</h6>
+              <TextFieldGroup
+                name="to"
+                type="date"
+                value={state.to}
+                onChange={onChange}
+                error={errors.to}
+                disabled={state.disabled ? "disabled" : ""}
+              />
+              <div className="form-check mb-4">
                 <input
-                  type="submit"
-                  value="Submit"
-                  className="btn btn-info btn-block mt-4"
+                  type="checkbox"
+                  className="form-check-input"
+                  name="current"
+                  value={state.current}
+                  checked={state.current}
+                  onChange={onCheck}
+                  id="current"
                 />
-              </form>
-            </div>
+                <label htmlFor="current" className="form-check-label">
+                  Ongoing
+                </label>
+              </div>
+              <TextAreaFieldGroup
+                placeholder="Program Description"
+                name="description"
+                value={state.description}
+                onChange={onChange}
+                error={errors.description}
+                info="Tell us about the program that you were in"
+              />
+              <input
+                type="submit"
+                value="Submit"
+                className="btn btn-info btn-block mt-4"
+              />
+            </form>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
 AddEducation.propTypes = {
+  addEducation: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired,
-  addEducation: PropTypes.func.isRequired,
 };
 
 //Comes from root reducer
